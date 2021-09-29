cca1b03adff2df4e00be4884fbf4dfcda31c50b5
Pulkit Dhingra <pulkit12dhingra@gmail.com>
2021-09-29 22:49:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ thread_lock = Lock()
 
 # Load the model
 net = gcv.model_zoo.get_model('yolo3_mobilenet0.25_coco', pretrained=True)
-net.hybridize()
+net.reset_class(classes=['bicycle', 'backpack','person'], reuse_weights=['bicycle', 'backpack','person'])
+
 mydict={}
 for i,name in enumerate(net.classes):
     mydict[i]=name
